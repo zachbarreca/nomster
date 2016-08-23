3d9bfb434d8abcd289e8439bd726e3d3c6d8f987
Zach Barreca <zachbarreca@protonmail.com>
2016-08-23 20:56:45 +0000

    Lesson 27 changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,8 +12,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+if @place.valid?
+  redirect_to root_path
+else
+  render :new, status: :unprocessable_entity
+end
   end
 
   def show
@@ -36,6 +40,11 @@ end
 
     @place.update_attributes(place_params)
     redirect_to root_path
+    if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
