7dff773ea4310da0b03dd4ece16ca99796ab104c
Zach Barreca <zachbarreca@protonmail.com>
2016-08-23 20:39:01 +0000

    Lesson 25 changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @places = Place.all
@@ -21,10 +21,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
   end
+end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+  
     @place.update_attributes(place_params)
     redirect_to root_path
   end
