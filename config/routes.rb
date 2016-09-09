5aa838fbd8809fe2ebfcdea54ae853c9aef49010
Zach Barreca <zachbarreca@protonmail.com>
2016-09-09 20:12:08 +0000

    lesson 33 changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'places#index'
+    resources :places do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'places#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
